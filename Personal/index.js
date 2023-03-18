39154c2bf9fd6c0a25848a92eafece0f20188aa6
Shubham <privatestartup2004@gmail.com>
2023-03-18 20:12:02 +0530

    Bug Fixed

--- Personal/index.js
+++ Personal/index.js
@@ -1,13 +1,13 @@
-// if (confirm("WARNING !!!\nYOU ARE ENTERING IN RESTRICTED AREA\nMALWARE, VIRUS AND DANGER AHEAD")) {
-//     if (confirm("WARNING !!! WARNING !!! WARNING !!! \nRESTRICTED AREA 💀\nARE YOU SURE ! \nWARNING !!! WARNING !!! WARNING !!!")) {
-//         if (confirm("FINAL WARNING !!!\nMALWARE, VIRUS AND DANGER ZONE AHEAD 💀")) {
-//             alert("WELCOME TO DANGER ZONE 💀\nMALWARE, VIRUS AND DANGER ZONE AHEAD 💀");
-//         } else {
-//             window.location.href = '/index.html';
-//         }
-//     } else {
-//         window.location.href = '/index.html';
-//     }
-// } else {
-//     window.location.href = '/index.html';
-// }
+if (confirm("WARNING !!!\nYOU ARE ENTERING IN RESTRICTED AREA\nMALWARE, VIRUS AND DANGER AHEAD")) {
+    if (confirm("WARNING !!! WARNING !!! WARNING !!! \nRESTRICTED AREA 💀\nARE YOU SURE ! \nWARNING !!! WARNING !!! WARNING !!!")) {
+        if (confirm("FINAL WARNING !!!\nMALWARE, VIRUS AND DANGER ZONE AHEAD 💀")) {
+            alert("WELCOME TO DANGER ZONE 💀\nMALWARE, VIRUS AND DANGER ZONE AHEAD 💀");
+        } else {
+            window.location.href = '/';
+        }
+    } else {
+        window.location.href = '/';
+    }
+} else {
+    window.location.href = '/';
+}
